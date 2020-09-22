0a7cd0b7b0880734c418d21a6e66bf65e3a94498
Jack Santry <Joseph.santry@outlook.com>
2020-09-22 10:02:50 -0700

    Add project files.

---
+++ Collections/Collections.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Collections
+    {
+        //My lists bring all the values to the yard...
+        public static HashSet<int> Hash()
+        {
+            HashSet<int> numbers = new HashSet<int>(new int[] { 1, 2, 3, 4, 5 });
+
+            return numbers;
+        }
+        static List<int> List()
+        {
+            List<int> list = new List<int>(5);
+
+            foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+            {
+                list.Add(number);
+            }
+
+            return list;
+        }
+        public static LinkedList<int> Link()
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+
+            foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+            {
+                numbers.AddFirst(number);
+            }
+            return numbers;
+        }
+        public static Queue<int> Que()
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+            {
+                numbers.Enqueue(number);
+            }
+            return numbers;
+
+        }
+        public static Stack<int> Stacker()
+        {
+            Stack<int> numbers = new Stack<int>();
+            foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+            {
+                numbers.Push(number);
+            }
+            return numbers;
+        }
+        public static Dictionary<string, int> Dict()
+        {
+            Dictionary<string, int> shoppingCart = new Dictionary<string, int>();
+
+            shoppingCart.Add("3080ti", 1);
+            shoppingCart.Add("RBG lights", 1000);
+            shoppingCart.Add("Paint Bucket, white", 5);
+            shoppingCart.Add("Plant feed", 2);
+
+            return shoppingCart;
+        }
+        public static SortedList<string, int> Sort()
+        {
+            SortedList<string, int> shoppingCart = new SortedList<string, int>();
+
+            shoppingCart.Add("3080ti", 1);
+            shoppingCart.Add("RBG lights", 1000);
+            shoppingCart.Add("Paint Bucket, white", 5);
+            shoppingCart.Add("Plant feed", 2);
+
+            return shoppingCart;
+        }
+
+        //The Yard
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collections Project");
+            Console.WriteLine("\nWith HashSet:");
+            foreach (var item in Hash())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith List:");
+            foreach (var item in List())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith LinkedList:");
+            foreach (var item in Link())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith Queue:");
+            foreach (var item in Que())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith Stack:");
+            foreach (var item in Stacker())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith Dictionary:");
+            foreach (var item in Dict())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nWith SortedList:");
+            foreach (var item in Sort())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ EX1B_421/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace EX1B_421
+{
+    public struct KeyValue<T>
+    {
+        public readonly string Key;
+        public readonly T Value;
+        public KeyValue(string key, T value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+
+public class MyDictionary
+    {
+        public KeyValue<int>[] keys = new KeyValue<int>[5];
+        public int nextIndex = 0;
+
+        public object this[string index]
+        {
+            get
+            {
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    if (index == keys[i].Key)
+                    {
+                        return keys[i].Value;
+                    }
+                }
+                throw new KeyNotFoundException("No Key Found!");
+
+            }
+            set
+
+            {
+                bool isfound = true;
+                for (int i = 0; i <= nextIndex; i++)
+                {
+                    if (this.keys[i].Key == index)
+                    {
+                        this.keys[i] = new KeyValue<int>(index, (int)value);
+                    }
+                    isfound = false;
+
+                }
+                if (isfound == false)
+                {
+                    keys[nextIndex] = new KeyValue<int>(index, (int)value);
+
+                }
+                nextIndex++;
+
+
+            }
+
+
+        }
+
+
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+            Console.ReadLine();
+        }
+    }
+}
